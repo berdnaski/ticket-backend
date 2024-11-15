5dc0976aecc96c9c5cc6c47457f3d46ba1f791ff
Erick Berdnaski <berdnaski1177@gmail.com>
2024-11-14 21:13:46 -0300

    initial

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express, { type Application } from 'express';
+
+class App {
+  public app: Application;
+  constructor() {
+    this.app = express();
+  }
+
+  listen() {
+    this.app.listen(3333, () => {
+      console.log('server is running');
+    })
+  }
+}
+export { App };
